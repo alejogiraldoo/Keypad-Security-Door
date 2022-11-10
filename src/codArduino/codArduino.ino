6b0e3235b413d31d361ab14c347e2a18a27f00ab
Alejandro Giraldo Ocampo <alejandrogiraldo1404@gmail.com>
2022-11-10 16:48:11 -0500

    Version basica del proyecto

---
+++ src/codArduino/codArduino.ino
@@ -0,0 +1,144 @@
+#include <LiquidCrystal_I2C.h> // Libreria para el manejo de la pantalla LCD.
+#include <Keypad.h> // Libreria para el manejo del teclado.
+#include <Servo.h> // Libreria para el manejo del servoMotor.
+// Permite trabajar con el hardware directamente del MODULO-GPS
+#include<SoftwareSerial.h>
+// Permite interpretar los datos enviados por el GPS(los datos son enviados en codigo NMEA -> National Marine Electronics Asociation)
+#include<TinyGPS.h>// TYNYGPS
+// VARIABLES
+char LetterKey; // Almacena la tecla presionada en el keypad
+int estado = 0;  //0:cerrado 1:abierto.
+char clave[5]; // Almacena la contraseña.
+char clave_maestra[5] = "1234"; // Almacena la contraseña a comparar.
+int indice; // indice del array
+int fallos = 0;
+int pinLed = 13;
+
+// COMPONENTES:
+//____________________PANTALLA LCD____________________
+LiquidCrystal_I2C lcd(0x27,16,2); // Creamos un objeto para controlar los parametros (INFO) que se le envia al LED
+
+//____________________KEYPAD____________________
+const byte ROWS = 4;
+const byte COLUMNS = 4;
+char keys[ROWS][COLUMNS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+byte rowsPins[ROWS] = {2,3,4,5};
+byte columnsPins[COLUMNS] = {6,7,8,9};
+
+Keypad teclado = Keypad(makeKeymap(keys),rowsPins,columnsPins,ROWS,COLUMNS);
+
+// ____________________SERVO MOTOR____________________
+int servoPin = 10;
+// Objeto que me permite controlar el servo Motor
+Servo myServo;
+
+// ____________________MODULO GPS____________________
+// DECLARAMOS UN OBJETO DE TIPO SOFTWARE-SERIAL
+SoftwareSerial ss(12,11); // TX/RX
+// DECLARAMOS UN OBJETO DE TIPO TINY-GPS
+TinyGPS gps;
+
+void setup() {
+  // PANTALLA LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("CLAVE:"); 
+  limpiar();
+  // SERVO MOTOR
+  myServo.attach(servoPin);
+  myServo.write(150);
+  // MODULO GPS
+  Serial.begin(115200);
+  // LE DECIMOS AL MODULO GPS QUE ME ENVIE LOS DATOS A LA VELOCIDAD DE 9600 baudios
+  ss.begin(9600);
+  // LED
+  pinMode(pinLed,OUTPUT);
+}
+
+void loop() {
+  LetterKey = teclado.getKey();
+  if (LetterKey){
+    if (strstr('#',LetterKey)){
+      limpiar();
+      indice = 0;
+    }else{
+      clave[indice] = LetterKey;
+      indice++;
+      lcd.print(LetterKey);
+    }
+  }
+
+  if (indice == 4){
+    if(!strcmp(clave,clave_maestra)){
+      abrir();
+    }else{
+       error();
+    }
+     indice = 0;
+  }
+}
+
+// ______Funcion limpiar______
+void limpiar(){
+  lcd.setCursor(0,0);
+  lcd.print("CLAVE:          ");
+  lcd.setCursor(9,0);
+}
+
+// ______Funcion abrir______
+void abrir(){
+  if (estado == 0){
+    estado = 1;
+    lcd.setCursor(0,1);
+    lcd.print("Abierto         ");
+    myServo.write(30);
+    fallos = 0;
+  }else{
+    estado = 0;
+    lcd.setCursor(0,1);
+    lcd.print("Cerrado         ");
+    myServo.write(150);
+  }
+  limpiar();
+  digitalWrite(pinLed,LOW);
+}
+
+// ______Funcion error______
+void error(){
+  if (estado == 0){
+    fallos += 1;
+    if (fallos == 3){
+      localizar();
+      fallos = 0;
+    }
+  }
+  
+  lcd.setCursor(0,1);
+  lcd.print("ERROR DE CLAVE  ");
+  limpiar();
+}
+
+// ______Funcion localizar______
+void localizar(){
+  digitalWrite(pinLed,HIGH);
+  // EL BUCLE SE EJECUTA SIEMPRE QUE SE ENVIE INFORMACIÓN
+  while(ss.available()>0){
+    // VERIFICA LA CONVERCIÓN DE INFORMACIÓN DE NMEA A COORDENADAS POLARES LEIBLES(LAT,LNG)
+    if(gps.encode(ss.read())){
+      // VARIABLES (LAT,LON)
+      float flat, flon;
+      gps.f_get_position(&flat, &flon);
+      //Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+      //Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+      Serial.println(String(flat,6) + "," + String(flon,6));
+    }
+  }
+}
---
+++ src/guardarExcel.py
@@ -0,0 +1,66 @@
+# LIBRERIA PARA REALIZAR LA CONEXION CON ARDUINO
+import serial # COMANDO PARA INSTALAR LA LIBRERIA : py -3.10 -m pip install pyserial
+import time
+# LIBRERIA PARA CREAR LA HOJA DE EXCEL
+import xlsxwriter # pip install xlsxwriter
+# LIBRERIA PARA CAPTURAR LA FECHA
+from datetime import datetime
+# LIBRERIA PARA CONVERTIR LA LATITUD Y LONGITUD A DIRECCION
+from geopy.geocoders import Nominatim
+
+
+# ___________________CONEXION CON ARDUINO___________________
+# PARAMETROS DEL PUERTO Y VELOCIDAD DE LOS DATOS
+serialArduino = serial.Serial("COM3",115200)
+# ESPERA UN SEGUNDO DESPUES DE CREAR EL OBJETO
+time.sleep(1)
+# ___________________HOJA EXCEL___________________
+archivo = xlsxwriter.Workbook('localizacion.xlsx')
+hoja = archivo.add_worksheet('hoja1')
+hoja.write(0,0,"FECHA")
+hoja.write(0,1,"LOCALIZACIÓN")
+cantRegistros = 1
+regisGuardados = 0
+row = 1
+
+while True:
+    # BUSCAMOS ERRORES EN CASO DE DAR ERRORES DE EJECUCIÓN
+    try:
+        # OBTENEMOS LA INFORMACIÓN DEL PUERTO
+        val = serialArduino.readline()
+        # DECODIFICAMOS LA INFORMACIÓN DEL PUERTO
+        cad = val.decode("ascii")
+        pos = cad.index(",")
+        # LAT Y LONG | BARRENDERO
+        lat = cad[:pos]
+        longt = cad[pos+1:]
+        # REALIZAMOS UNA VERIFICACIÓN PARA VALIDAR QUE LAS COORDENADAS FUERON ENVIADAS COMPLETAS
+        if len(lat) >= 8 and len(longt) >= 8:
+            # CONTAMOS LA CANTIDAD DE REGISTROS GUARDADOS
+            regisGuardados += 1
+            # CAPTURAMOS LA FECHA ACTUAL
+            fecha = datetime.now()
+            # CONVERTIMOS LAS COORDENADAS EN DIRECCIÓN
+            geolocator = Nominatim(user_agent="ubicEntrada")
+            location = geolocator.reverse(f"{lat},{longt}")
+            datos = [str(fecha),location.address]
+            # INSERTAMOS LOS DATOS EN LA HOJA DE EXCEL
+            column = 0
+            for dato in datos:
+                hoja.write(row,column,dato)
+                column += 1
+            row += 1
+            # IMPRIMIMOS LO QUE GUARDAMOS
+            #print(f"{lat} {longt}") #print(location.address)
+            
+            # COMPARAMOS SI LOS REGISTROS GUARDADOS SON IGUAL A LOS REQUERIDOS
+            if cantRegistros == regisGuardados:
+                # GUARDAMOS LA CANTIDAD DE ARCHIVOS
+                archivo.close()
+                break
+    except:
+        #print("ERROR AL ENVIAR LOS DATOS DESDE EL PUERTO...")
+        pass
+
+
+
